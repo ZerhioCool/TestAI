572b974e99da2c2a93410ae24dceae0c1f71f215
ZerhioCool <celebracionsara80@gmail.com>
2026-04-09 11:50:27 -0400

    Update route.ts

--- src/app/api/quiz/generate/route.ts
+++ src/app/api/quiz/generate/route.ts
@@ -56,7 +56,7 @@ export async function POST(req: NextRequest) {
     }
 
     const model = genAI.getGenerativeModel({ 
-      model: "gemini-2.0-flash-lite",
+      model: "	gemini-3.1-flash-lite-preview",
       generationConfig: {
         responseMimeType: "application/json",
       }
